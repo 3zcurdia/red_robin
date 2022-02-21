b5e6a6c80ba1674e86b358580754b35076ae5bbd
Luis Ezcurdia <ing.ezcurdia@gmail.com>
2022-02-21 15:04:00 -0600

    Add basic dependencies

--- mix.exs
+++ mix.exs
@@ -11,20 +11,11 @@ defmodule RedRobin.Umbrella.MixProject do
     ]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options.
-  #
-  # Dependencies listed here are available only for this project
-  # and cannot be accessed from applications inside the apps/ folder.
   defp deps do
-    []
+    [
+      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.28.1", only: :dev, runtime: false}
+    ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
